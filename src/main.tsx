11935003fc0b9bd0783392e11e9e406d7e20ab6d
Aradhy <bajpaiaradhy@gmail.com>
2025-05-01 20:43:28 +0530

    Production bug 01 updated

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { ClerkProvider } from "@clerk/clerk-react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+
+import { ClerkProvider } from "@clerk/clerk-react";
 
 import "./index.css";
 import App from "./App.tsx";
 import { ToasterProvider } from "./provider/toast-provider.tsx";
-import React from "react";
 
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
@@ -13,15 +14,11 @@ if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key");
 }
 
-const isDev = import.meta.env.DEV;
-
-const RootComponent = (
-  <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
-    <App />
-    <ToasterProvider />
-  </ClerkProvider>
-);
-
 createRoot(document.getElementById("root")!).render(
-  isDev ? <React.StrictMode>{RootComponent}</React.StrictMode> : RootComponent
+  <StrictMode>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+      <App />
+      <ToasterProvider />
+    </ClerkProvider>
+  </StrictMode>
 );
